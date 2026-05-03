0b695cffa2a64d67eefd92d6ba5bf1b984544ba4
iamdipanshugupta <kumardipanshu983542@gmail.com>
2026-05-03 13:52:18 +0530

    Change default server port from 8000 to 3000

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ async function main() {
         return res.json({ checkboxes: JSON.parse(state) });
     });
 
-    const PORT = process.env.PORT ?? 8000;
+    const PORT = process.env.PORT ?? 3000;
     server.listen(PORT, () => {
         console.log(`Server running on http://localhost:${PORT}`);
     });
