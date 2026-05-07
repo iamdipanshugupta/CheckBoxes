888e0bc73fa7b47ee250ca0cc6f94f46aa5feac5
iamdipanshugupta <kumardipanshu983542@gmail.com>
2026-05-08 00:56:44 +0530

    Import dotenv and configure environment variables for Redis connection

--- redis-connection.js
+++ redis-connection.js
@@ -1,5 +1,6 @@
 import Redis from "ioredis";
-
+import dotenv from 'dotenv';
+dotenv.config();
 function createRedisConnection() {
     return new Redis(process.env.REDIS_URL, {
         maxRetriesPerRequest: null,
